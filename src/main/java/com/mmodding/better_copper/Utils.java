fe127c057c6bc0433be59fd18f83d7148ea67e74
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2022-08-21 04:43:22 +0200

    Started to add Copper power block

--- src/main/java/com/mmodding/better_copper/Utils.java
+++ src/main/java/com/mmodding/better_copper/Utils.java
@@ -1,6 +1,13 @@
 package com.mmodding.better_copper;
 
+import com.mmodding.better_copper.init.Blocks;
+import net.minecraft.block.OxidizableBlock;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class Utils {
 
@@ -10,4 +17,20 @@ public class Utils {
 		return new Identifier(Utils.modIdentifier, path);
 	}
 
+	@Nullable
+	public static BlockPos getLinkedPowerBlock(World world, BlockPos pos, Direction[] directions, int i) {
+		if (i >= 200) return null;
+		for (Direction dir : directions) {
+			BlockPos otherPos = pos.offset(dir);
+			if (world.getBlockState(otherPos).getBlock() instanceof OxidizableBlock) {
+				if (world.getBlockState(otherPos).getBlock() == Blocks.COPPER_POWER_BLOCK) return otherPos;
+				return getLinkedPowerBlock(world, otherPos, directions, i + 6);
+			}
+		}
+		return null;
+	}
+
+	public static boolean isInNetwork(World world, BlockPos pos, Direction[] directions) {
+		return getLinkedPowerBlock(world, pos, directions, 0) != null;
+	}
 }
--- src/main/java/com/mmodding/better_copper/init/Blocks.java
+++ src/main/java/com/mmodding/better_copper/init/Blocks.java
@@ -1,12 +1,19 @@
 package com.mmodding.better_copper.init;
 
+import com.mmodding.better_copper.Utils;
+import com.mmodding.mmodding_lib.library.blocks.CustomBlock;
 import com.mmodding.mmodding_lib.library.initializers.ClientElementsInitializer;
 import com.mmodding.mmodding_lib.library.initializers.ElementsInitializer;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.item.ItemGroup;
 
 public class Blocks implements ElementsInitializer, ClientElementsInitializer {
 
+	public static final CustomBlock COPPER_POWER_BLOCK = new CustomBlock(AbstractBlock.Settings.copy(net.minecraft.block.Blocks.COPPER_BLOCK), true, ItemGroup.BUILDING_BLOCKS);
+
 	@Override
 	public void register() {
+		COPPER_POWER_BLOCK.register(Utils.newIdentifier("copper_power_block"));
 	}
 
 	@Override
