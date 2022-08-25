f62e24b15b159b89537eabcbf9704703fdf07df5
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-08-25 22:45:07 +0200

    Update MModding Library

--- src/main/java/com/mmodding/better_copper/BetterCopper.java
+++ src/main/java/com/mmodding/better_copper/BetterCopper.java
@@ -5,7 +5,9 @@ import com.mmodding.better_copper.init.Blocks;
 import com.mmodding.better_copper.init.Items;
 import com.mmodding.mmodding_lib.library.base.MModdingModContainer;
 import com.mmodding.mmodding_lib.library.base.MModdingModInitializer;
+import com.mmodding.mmodding_lib.library.config.Config;
 import com.mmodding.mmodding_lib.library.initializers.ElementsInitializer;
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.ModContainer;
 
 import java.util.ArrayList;
@@ -22,6 +24,12 @@ public class BetterCopper implements MModdingModInitializer {
 		initializers.add(new BlockEntities());
 		initializers.add(new Items());
 		return initializers;
+	}
+
+	@Nullable
+	@Override
+	public Config getConfig() {
+		return null;
 	}
 
 	@Override
--- src/main/java/com/mmodding/better_copper/client/BetterCopperClient.java
+++ src/main/java/com/mmodding/better_copper/client/BetterCopperClient.java
@@ -4,10 +4,12 @@ import com.mmodding.better_copper.client.render.Outliner;
 import com.mmodding.better_copper.client.render.PowerValueRenderer;
 import com.mmodding.better_copper.init.Blocks;
 import com.mmodding.mmodding_lib.library.base.MModdingClientModInitializer;
+import com.mmodding.mmodding_lib.library.config.Config;
 import com.mmodding.mmodding_lib.library.initializers.ClientElementsInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.lifecycle.api.client.event.ClientTickEvents;
 
@@ -24,6 +26,12 @@ public class BetterCopperClient implements MModdingClientModInitializer {
 		List<ClientElementsInitializer> clientInitializers = new ArrayList<>();
 		clientInitializers.add(new Blocks());
 		return clientInitializers;
+	}
+
+	@Nullable
+	@Override
+	public Config getClientConfig() {
+		return null;
 	}
 
 	@Override
