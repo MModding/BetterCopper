8dbc9c99c0ff49a149d6b31ee53b1c6fffda621f
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2022-08-30 03:04:34 +0200

    Fixed unwanted changes

--- src/main/java/com/mmodding/better_copper/mixin/EntityMixin.java
+++ src/main/java/com/mmodding/better_copper/mixin/EntityMixin.java
@@ -7,12 +7,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
 
 	@Shadow
-	public BlockPos getBlockPos() {
-		return null;
-	}
+	abstract public BlockPos getBlockPos();
 
 	@Shadow
 	public World world;
--- src/main/java/com/mmodding/better_copper/mixin/generation/FallingBlockEntityMixin.java
+++ src/main/java/com/mmodding/better_copper/mixin/generation/FallingBlockEntityMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FallingBlockEntity.class)
-public class FallingBlockEntityMixin extends EntityMixin implements TickOperations {
+public abstract class FallingBlockEntityMixin extends EntityMixin implements TickOperations {
 
 	@Shadow
 	private BlockState block;
